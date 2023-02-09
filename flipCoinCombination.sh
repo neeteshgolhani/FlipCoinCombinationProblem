5bc29f72682eb5976370c66c7a0e9fabb4dc9d6f
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-02-10 03:48:22 +0530

    Add:As a Simulator start with Flipping a Coin to Display Heads or Tails - Use ((RANDOM)) to find Heads or Tails

---
+++ flipCoinCombination.sh
@@ -0,0 +1,7 @@
+echo "This problem displays winning percentage of Head or Tail toss=$(( RANDOM % 2 ))
+if(( toss == 0 ))
+then
+    echo "Heads"
+else
+    echo "Tails"
+fiCombination in a Singlet, Doublet and Triplet"
